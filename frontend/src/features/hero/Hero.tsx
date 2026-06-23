c386648c0fba4b8e3f166df3a6db03e0cfecc35c
Aryankumar12 <aryandot1@gmail.com>
2026-06-23 11:35:20 +0530

    feat(hero): add hero content and buttons

--- frontend/src/features/hero/Hero.tsx
+++ frontend/src/features/hero/Hero.tsx
@@ -1,17 +1,28 @@
 import Button from "../../components/Button";
-function Hero(){
+function Hero() {
+  function handleResumeClick(): void {
+    alert("hellow i clieck");
+  }
+  function handleContactClick(): void {
+    alert("haha");
+  }
 
-    function handleClick():void{
-        alert("hellow i clieck")
-    }
-return (
+  const name = "Aryan Kumar";
+  const role = "Software Engineer | Full Stack Devloper";
+  const introduction = "I am 2026 grad currently will work in Epam System as junior software Engineer";
+  return (
     <>
+        <div>
 
-    <h1>This is the hero section </h1>
-    <Button text="Downlaod" onClick={handleClick}/>
-    
+        <h1>{name}</h1>
+        <p>{role}</p>
+        <p>{introduction}</p>
+        </div>
+
+      <Button text="Download Resume" onClick={handleResumeClick} />
+      <Button text="Contact Me" onClick={handleContactClick} />
     </>
-)
+  );
 }
 
 export default Hero;
