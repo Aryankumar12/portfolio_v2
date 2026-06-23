ffd40266b7519abbb8018bdc1b419396a2cc670a
Aryankumar12 <aryandot1@gmail.com>
2026-06-23 17:44:23 +0530

    feat(navbar): add navigation link metadata

--- frontend/src/features/navbar/Navbar.tsx
+++ frontend/src/features/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
 const Navbar = () => {
-  const navLinks = ["Projects", "Skills", "Resume", "Contact"];
+  const navLinks = [
+    {label : "Projects", href: "#projects"}, {label : "Skills", href: "#skills"}, {label : "Resume", href: "#resume"} , {label : "Contact", href: "#contact"}
+  ]
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <div className=" bg-black text-white flex justify-between items-center py-7 px-6 md:px-12 sticky top-0 z-50">
+      <nav className=" bg-black text-white flex justify-between items-center py-7 px-6 md:px-12 sticky top-0 z-50">
         <div className="logo text-xl font-bold">Aryan Kumar</div>
 
         <div className="links">
@@ -14,23 +16,24 @@ const Navbar = () => {
             onClick={() => setIsOpen(!isOpen)}
             className="sm:hidden block text-zinc-400 hover:text-white cursor-pointer text-xl font-bold"
           >
-            {isOpen ? "X" : "="}
+            {isOpen ? "X" : "☰"}
           </button>
          
           <ul className="hidden sm:flex gap-6 justify-center text-center">
             {navLinks.map((x) => {
               return (
                 <li
-                  key={x}
+                  key={x.label}
                   className="cursor-pointer text-zinc-400 hover:text-white text-md font-medium transition-colors duration-200"
+                  
                 >
-                  {x}
+                  <a href={x.href}> {x.label}</a>
                 </li>
               );
             })}
           </ul>
         </div>
-      </div>
+      </nav>
 
       {isOpen && (
         <ul className=" absolute right-0 top-20 w-1/2 flex flex-col items-center gap-2 bg-black text-white py-6 border-t border-zinc-800 z-40 sm:hidden">
