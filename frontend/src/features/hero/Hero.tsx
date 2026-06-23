f998b9d42698b2856638993fb77386785f6e0dad
Aryankumar12 <aryandot1@gmail.com>
2026-06-23 11:53:40 +0530

    refactor(hero): group hero content into objects

--- frontend/src/features/hero/Hero.tsx
+++ frontend/src/features/hero/Hero.tsx
@@ -7,20 +7,24 @@ function Hero() {
     alert("haha");
   }
 
-  const name = "Aryan Kumar";
-  const role = "Software Engineer | Full Stack Devloper";
-  const introduction = "I am 2026 grad currently will work in Epam System as junior software Engineer";
+ 
+  const heroData = {
+    name: "Aryan Kumar",
+    role: "Software Engineer | Full Stack Devloper", 
+    introduction: "I am 2026 grad currently will work in Epam System as junior software Engineer"
+  }
   return (
     <>
         <div>
 
-        <h1>{name}</h1>
-        <p>{role}</p>
-        <p>{introduction}</p>
+        <h1>{heroData.name}</h1>
+        <p>{heroData.role}</p>
+        <p>{heroData.introduction}</p>
         </div>
 
       <Button text="Download Resume" onClick={handleResumeClick} />
       <Button text="Contact Me" onClick={handleContactClick} />
+
     </>
   );
 }
