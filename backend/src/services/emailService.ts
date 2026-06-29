4c0bd60c4507e8fed226e1609fbf202bab7c7bc9
Aryankumar12 <aryandot1@gmail.com>
2026-06-29 13:55:03 +0530

    fix: ports in emailservice

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import SMTPConnection from "nodemailer/lib/smtp-connection";
 
 interface EmailPayload {
   name: string;
@@ -11,7 +12,9 @@ export const sendEmail = async ({ name, email, message }: EmailPayload) => {
   console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "exists" : "undefined"); // debug
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host:"smtp.gmail.com",
+    port: 587,
+    secure:false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
