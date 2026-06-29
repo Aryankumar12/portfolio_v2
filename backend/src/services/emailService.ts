49d61197b0e11c2fbca774d4bcea8b65cf86a4d9
Aryankumar12 <aryandot1@gmail.com>
2026-06-29 13:46:39 +0530

    fix: emailservice

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -1,34 +1,33 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-import path from "path";
+
 interface EmailPayload {
   name: string;
   email: string;
   message: string;
 }
-dotenv.config({ path: ".env" });
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+export const sendEmail = async ({ name, email, message }: EmailPayload) => {
+  console.log("EMAIL_USER:", process.env.EMAIL_USER); 
+  console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "exists" : "undefined"); // debug
 
-// console.log("EMAIL_USER:", process.env.EMAIL_USER);
-// console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
-export const sendEmail = async({name, email, message}:EmailPayload)=>{
-    await transporter.sendMail({
-         from:email,
-    to: process.env.EMAIL_USER, 
-     replyTo: email, subject: `Portfolio Contact — ${name}`,
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    replyTo: email,
+    subject: `Portfolio Contact — ${name}`,
     html: `
       <h3>New message from your portfolio</h3>
       <p><strong>Name:</strong> ${name}</p>
       <p><strong>Email:</strong> ${email}</p>
       <p><strong>Message:</strong> ${message}</p>
     `,
-    })
-
-}
+  });
+};
